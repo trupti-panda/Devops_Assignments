e580a974661c7a1a302419c3accc8098d7ce073e
EC2 Default User <ec2-user@ip-172-31-36-223.us-west-2.compute.internal>
2025-04-30 08:52:17 +0000

    Added TASK-19

---
+++ TASK-19/terraform/prod.tfvars
@@ -0,0 +1,6 @@
+environment = "prod"
+instance_count = 1
+instance_type = "t2.micro"
+key_name =  "MC-PUBLIC"
+ami_id = "ami-0f4fe28e9d0154aec"
+
---
+++ TASK-19/terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {}
+variable "ami_id" {}
+variable "environment" {}
+variable "key_name" {}
+variable "instance_count" {}
+
